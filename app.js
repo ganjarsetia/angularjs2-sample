549ea1ea5c8846fd3c94ab77b7d5d9a88190f4aa
Ganjar Setia <ganjar_setia@ymail.com>
2015-10-27 21:22:42 +0700

    add second component

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 (function() {
+
+	var MyName = 
+		ng
+		.Component({
+			selector: 'my-name',
+			template: '<span>Ganjar Setia M</span>'
+		})
+		.Class({
+			constructor: function() {}
+		});
 	
 	var HelloApp = 
 		ng
 		.Component({
 			selector: 'hello-app',
-			template: '<h1>Hello Angular 2!</h1>',
+			template: '<h1>Hello Angular 2!</h1><my-name></my-name>',
+			directives: [MyName]
 		})
 		.Class({
 			constructor: function() {}
